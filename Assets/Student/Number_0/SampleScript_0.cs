b34c3572a11c8d0ad2db3db891fcfc840de00a15
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-19 22:52:56 +0900

    [add]ナンバー0のテンプレフォルダを追加

--- Assets/Student/Number_0/SampleScript_0.cs
+++ Assets/Student/Number_0/SampleScript_0.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class SampleScript_0 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string _text = "text";
+  
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(_text);
     }
 }
