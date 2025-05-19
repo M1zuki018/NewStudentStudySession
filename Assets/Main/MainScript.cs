c59a33b3d480c674be602d74fa61a98f0a9bbecd
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-19 22:45:27 +0900

    [add]メイン系のフォルダを追加

---
+++ Assets/Main/MainScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public void Start()
+    {
+         Method_0();
+    }
+
+    
+    
+    private void Method_0()
+    {
+        Debug.Log("Method_0");
+    }
+}
