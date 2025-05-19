e2ed30101dd28eca7a8edcf5145411b62ed7b897
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-20 02:37:50 +0900

    [update]リターン方式に変更

--- Assets/Main/MainScript.cs
+++ Assets/Main/MainScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 public class MainScript : MonoBehaviour
 {
     # region 全てのメソッドを実行する
@@ -6,67 +7,75 @@ public class MainScript : MonoBehaviour
     {
         CenterMethod();
         
-        Method_0();
-        Method_1();
-        Method_2();
-        Method_3();
-        Method_4();
-        Method_5();
-        Method_6();
-        Method_7();
-        Method_8();
-        Method_9();
-        Method_10();
-        Method_11();
-        Method_12();
-        Method_13();
-        Method_14();
-        Method_15();
-        Method_16();
-        Method_17();
-        Method_18();
-        Method_19();
-        Method_20();
-        Method_21();
-        Method_22();
-        Method_23();
-        Method_24();
-        Method_25();
-        Method_26();
-        Method_27();
-        Method_28();
-        Method_29();
-        Method_30();
-        Method_31();
-        Method_32();
-        Method_33();
-        Method_34();
-        Method_35();
-        Method_36();
-        Method_37();
-        Method_38();
-        Method_39();
-        Method_40();
-        Method_41();
-        Method_42();
-        Method_43();
-        Method_44();
-        Method_45();
-        Method_46();
-        Method_47();
-        Method_48();
-        Method_49();
-        Method_50();
-        Method_51();
-        Method_52();
-        Method_53();
-        Method_54();
-        Method_55();
-        Method_56();
-        Method_57();
-        Method_58();
-        Method_59();
-        Method_60();
+        // 結果を格納する配列を作成
+        string[] results = new string[61];
+        
+        // 各メソッドの結果を配列に格納
+        results[0] = Method_0();
+        results[1] = Method_1();
+        results[2] = Method_2();
+        results[3] = Method_3();
+        results[4] = Method_4();
+        results[5] = Method_5();
+        results[6] = Method_6();
+        results[7] = Method_7();
+        results[8] = Method_8();
+        results[9] = Method_9();
+        results[10] = Method_10();
+        results[11] = Method_11();
+        results[12] = Method_12();
+        results[13] = Method_13();
+        results[14] = Method_14();
+        results[15] = Method_15();
+        results[16] = Method_16();
+        results[17] = Method_17();
+        results[18] = Method_18();
+        results[19] = Method_19();
+        results[20] = Method_20();
+        results[21] = Method_21();
+        results[22] = Method_22();
+        results[23] = Method_23();
+        results[24] = Method_24();
+        results[25] = Method_25();
+        results[26] = Method_26();
+        results[27] = Method_27();
+        results[28] = Method_28();
+        results[29] = Method_29();
+        results[30] = Method_30();
+        results[31] = Method_31();
+        results[32] = Method_32();
+        results[33] = Method_33();
+        results[34] = Method_34();
+        results[35] = Method_35();
+        results[36] = Method_36();
+        results[37] = Method_37();
+        results[38] = Method_38();
+        results[39] = Method_39();
+        results[40] = Method_40();
+        results[41] = Method_41();
+        results[42] = Method_42();
+        results[43] = Method_43();
+        results[44] = Method_44();
+        results[45] = Method_45();
+        results[46] = Method_46();
+        results[47] = Method_47();
+        results[48] = Method_48();
+        results[49] = Method_49();
+        results[50] = Method_50();
+        results[51] = Method_51();
+        results[52] = Method_52();
+        results[53] = Method_53();
+        results[54] = Method_54();
+        results[55] = Method_55();
+        results[56] = Method_56();
+        results[57] = Method_57();
+        results[58] = Method_58();
+        results[59] = Method_59();
+        results[60] = Method_60();
+        
+        // 配列の内容を改行区切りで結合して出力
+        string output = string.Join('\n', results);
+        Debug.Log(output);
     }
 
     #endregion
@@ -76,308 +85,308 @@ public class MainScript : MonoBehaviour
         Debug.Log("this is the center method");
     }
     
-    private void Method_0()
+    private string Method_0()
     {
-        Debug.Log("Method_0");
+        return "number is 0";
     }
     
-    private void Method_1()
+    private string Method_1()
     {
-        Debug.Log("Method_1");
+        return "number is 1";
     }
     
-    private void Method_2()
+    private string Method_2()
     {
-        Debug.Log("Method_2");
+        return "number is 2";
     }
     
-    private void Method_3()
+    private string Method_3()
     {
-        Debug.Log("Method_3");
+        return "number is 3";
     }
     
-    private void Method_4()
+    private string Method_4()
     {
-        Debug.Log("Method_4");
+        return "number is 4";
     }
     
-    private void Method_5()
+    private string Method_5()
     {
-        Debug.Log("Method_5");
+        return "number is 5";
     }
     
-    private void Method_6()
+    private string Method_6()
     {
-        Debug.Log("Method_6");
+        return "number is 6";
     }
     
-    private void Method_7()
+    private string Method_7()
     {
-        Debug.Log("Method_7");
+        return "number is 7";
     }
     
-    private void Method_8()
+    private string Method_8()
     {
-        Debug.Log("Method_8");
+        return "number is 8";
     }
     
-    private void Method_9()
+    private string Method_9()
     {
-        Debug.Log("Method_9");
+        return "number is 9";
     }
     
-    private void Method_10()
+    private string Method_10()
     {
-        Debug.Log("Method_10");
+        return "number is 10";
     }
     
-    private void Method_11()
+    private string Method_11()
     {
-        Debug.Log("Method_11");
+        return "number is 11";
     }
     
-    private void Method_12()
+    private string Method_12()
     {
-        Debug.Log("Method_12");
+        return "number is 12";
     }
     
-    private void Method_13()
+    private string Method_13()
     {
-        Debug.Log("Method_13");
+        return "number is 13";
     }
     
-    private void Method_14()
+    private string Method_14()
     {
-        Debug.Log("Method_14");
+        return "number is 14";
     }
     
-    private void Method_15()
+    private string Method_15()
     {
-        Debug.Log("Method_15");
+        return "number is 15";
     }
     
-    private void Method_16()
+    private string Method_16()
     {
-        Debug.Log("Method_16");
+        return "number is 16";
     }
     
-    private void Method_17()
+    private string Method_17()
     {
-        Debug.Log("Method_17");
+        return "number is 17";
     }
     
-    private void Method_18()
+    private string Method_18()
     {
-        Debug.Log("Method_18");
+        return "number is 18";
     }
     
-    private void Method_19()
+    private string Method_19()
     {
-        Debug.Log("Method_19");
+        return "number is 19";
     }
     
-    private void Method_20()
+    private string Method_20()
     {
-        Debug.Log("Method_20");
+        return "number is 20";
     }
     
-    private void Method_21()
+    private string Method_21()
     {
-        Debug.Log("Method_21");
+        return "number is 21";
     }
     
-    private void Method_22()
+    private string Method_22()
     {
-        Debug.Log("Method_22");
+        return "number is 22";
     }
     
-    private void Method_23()
+    private string Method_23()
     {
-        Debug.Log("Method_23");
+        return "number is 23";
     }
     
-    private void Method_24()
+    private string Method_24()
     {
-        Debug.Log("Method_24");
+        return "number is 24";
     }
     
-    private void Method_25()
+    private string Method_25()
     {
-        Debug.Log("Method_25");
+        return "number is 25";
     }
     
-    private void Method_26()
+    private string Method_26()
     {
-        Debug.Log("Method_26");
+        return "number is 26";
     }
     
-    private void Method_27()
+    private string Method_27()
     {
-        Debug.Log("Method_27");
+        return "number is 27";
     }
     
-    private void Method_28()
+    private string Method_28()
     {
-        Debug.Log("Method_28");
+        return "number is 28";
     }
     
-    private void Method_29()
+    private string Method_29()
     {
-        Debug.Log("Method_29");
+        return "number is 29";
     }
     
-    private void Method_30()
+    private string Method_30()
     {
-        Debug.Log("Method_30");
+        return "number is 30";
     }
     
-    private void Method_31()
+    private string Method_31()
     {
-        Debug.Log("Method_31");
+        return "number is 31";
     }
     
-    private void Method_32()
+    private string Method_32()
     {
-        Debug.Log("Method_32");
+        return "number is 32";
     }
     
-    private void Method_33()
+    private string Method_33()
     {
-        Debug.Log("Method_33");
+        return "number is 33";
     }
     
-    private void Method_34()
+    private string Method_34()
     {
-        Debug.Log("Method_34");
+        return "number is 34";
     }
     
-    private void Method_35()
+    private string Method_35()
     {
-        Debug.Log("Method_35");
+        return "number is 35";
     }
     
-    private void Method_36()
+    private string Method_36()
     {
-        Debug.Log("Method_36");
+        return "number is 36";
     }
     
-    private void Method_37()
+    private string Method_37()
     {
-        Debug.Log("Method_37");
+        return "number is 37";
     }
     
-    private void Method_38()
+    private string Method_38()
     {
-        Debug.Log("Method_38");
+        return "number is 38";
     }
     
-    private void Method_39()
+    private string Method_39()
     {
-        Debug.Log("Method_39");
+        return "number is 39";
     }
     
-    private void Method_40()
+    private string Method_40()
     {
-        Debug.Log("Method_40");
+        return "number is 40";
     }
     
-    private void Method_41()
+    private string Method_41()
     {
-        Debug.Log("Method_41");
+        return "number is 41";
     }
     
-    private void Method_42()
+    private string Method_42()
     {
-        Debug.Log("Method_42");
+        return "number is 42";
     }
     
-    private void Method_43()
+    private string Method_43()
     {
-        Debug.Log("Method_43");
+        return "number is 43";
     }
     
-    private void Method_44()
+    private string Method_44()
     {
-        Debug.Log("Method_44");
+        return "number is 44";
     }
     
-    private void Method_45()
+    private string Method_45()
     {
-        Debug.Log("Method_45");
+        return "number is 45";
     }
     
-    private void Method_46()
+    private string Method_46()
     {
-        Debug.Log("Method_46");
+        return "number is 46";
     }
     
-    private void Method_47()
+    private string Method_47()
     {
-        Debug.Log("Method_47");
+        return "number is 47";
     }
     
-    private void Method_48()
+    private string Method_48()
     {
-        Debug.Log("Method_48");
+        return "number is 48";
     }
     
-    private void Method_49()
+    private string Method_49()
     {
-        Debug.Log("Method_49");
+        return "number is 49";
     }
     
-    private void Method_50()
+    private string Method_50()
     {
-        Debug.Log("Method_50");
+        return "number is 50";
     }
     
-    private void Method_51()
+    private string Method_51()
     {
-        Debug.Log("Method_51");
+        return "number is 51";
     }
     
-    private void Method_52()
+    private string Method_52()
     {
-        Debug.Log("Method_52");
+        return "number is 52";
     }
     
-    private void Method_53()
+    private string Method_53()
     {
-        Debug.Log("Method_53");
+        return "number is 53";
     }
     
-    private void Method_54()
+    private string Method_54()
     {
-        Debug.Log("Method_54");
+        return "number is 54";
     }
     
-    private void Method_55()
+    private string Method_55()
     {
-        Debug.Log("Method_55");
+        return "number is 55";
     }
     
-    private void Method_56()
+    private string Method_56()
     {
-        Debug.Log("Method_56");
+        return "number is 56";
     }
     
-    private void Method_57()
+    private string Method_57()
     {
-        Debug.Log("Method_57");
+        return "number is 57";
     }
     
-    private void Method_58()
+    private string Method_58()
     {
-        Debug.Log("Method_58");
+        return "number is 58";
     }
     
-    private void Method_59()
+    private string Method_59()
     {
-        Debug.Log("Method_59");
+        return "number is 59";
     }
     
-    private void Method_60()
+    private string Method_60()
     {
-        Debug.Log("Method_60");
+        return "number is 60";
     }
 }
