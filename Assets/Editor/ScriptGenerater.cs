c9a69c190414c126e96fa998a6da1aae7aec400f
tsutsumikiriku <tsutsumikiriku@gmail.com>
2025-05-19 23:51:53 +0900

    [add]スクリプトジェネレーターの作成

---
+++ Assets/Editor/ScriptGenerater.cs
@@ -0,0 +1,82 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class ScriptGenerator : EditorWindow
+{
+    string _numberOfPeople  = "60";
+
+    [MenuItem("Tools/Script Generator")]
+    public static void ShowWindow()
+    {
+        GetWindow<ScriptGenerator>("Script Generator");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("MonoBehaviourスクリプトを作成できます", EditorStyles.boldLabel);
+        _numberOfPeople= EditorGUILayout.TextField("何人分のスクリプトを作成しますか？", _numberOfPeople);
+        if (GUILayout.Button("スクリプトを作成します"))
+        {
+            GenerateScript(_numberOfPeople);
+        }
+    }
+    void GenerateScript(string numberOfPeople)
+    {
+        int count = int.Parse(numberOfPeople);
+        for (int i = 0; i < count; i++)
+        {
+            string filename = "Number_" + (i + 1).ToString();
+            string classname = "SampleScript_" + (i + 1).ToString();
+            string folderPath = "Assets/Student/" + filename;
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            string scriptPath = Path.Combine(folderPath, classname + ".cs");
+            if (File.Exists(scriptPath))
+            {
+                Debug.LogWarning("Script already exists: " + scriptPath);
+                return;
+            }
+string classTemplate = 
+$@"using UnityEngine;
+
+public class {classname} : MonoBehaviour
+{{
+    [Header(""パラメータ"")]
+    
+    [SerializeField] private string _text = ""text"";
+    
+    #region テキスト表示のパラメータ
+    
+    [Header(""テキストの表示"")]
+    
+    [SerializeField, Range(1, 512)] private int _fontSize = 128;
+    [SerializeField] private Color _color = Color.white;
+    [SerializeField] private TextAnchor _anchor = TextAnchor.MiddleCenter;
+
+    #endregion
+    private void OnGUI()
+    {{ 
+            // フォントサイズなどのスタイルを設定
+            GUIStyle style = new(GUI.skin.label);
+            style.fontSize = _fontSize;
+            style.normal.textColor = _color;
+            style.alignment = _anchor;
+
+            // 画面の中央に配置するRect
+            Rect rect = new Rect(
+                position: Vector2.zero,
+                size: new Vector2(Screen.width, Screen.height)
+            );
+
+            // テキストを描画
+            GUI.Label(rect, _text, style);
+        }}
+}}";
+        File.WriteAllText(scriptPath, classTemplate);
+        AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/Student/Number_33/SampleScript_33.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SampleScript_33 : MonoBehaviour
+{
+    [Header("パラメータ")]
+    
+    [SerializeField] private string _text = "text";
+    
+    #region テキスト表示のパラメータ
+    
+    [Header("テキストの表示")]
+    
+    [SerializeField, Range(1, 512)] private int _fontSize = 128;
+    [SerializeField] private Color _color = Color.white;
+    [SerializeField] private TextAnchor _anchor = TextAnchor.MiddleCenter;
+
+    #endregion
+    private void OnGUI()
+    { 
+            // フォントサイズなどのスタイルを設定
+            GUIStyle style = new(GUI.skin.label);
+            style.fontSize = _fontSize;
+            style.normal.textColor = _color;
+            style.alignment = _anchor;
+
+            // 画面の中央に配置するRect
+            Rect rect = new Rect(
+                position: Vector2.zero,
+                size: new Vector2(Screen.width, Screen.height)
+            );
+
+            // テキストを描画
+            GUI.Label(rect, _text, style);
+        }
+}
