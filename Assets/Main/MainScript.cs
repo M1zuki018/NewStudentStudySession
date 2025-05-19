6055788873ef54bc8de2c7e8fc16695603f608e8
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-20 01:48:11 +0900

    [update]MainScriptを作成

--- Assets/Main/MainScript.cs
+++ Assets/Main/MainScript.cs
@@ -1,16 +1,383 @@
 using UnityEngine;
-
 public class MainScript : MonoBehaviour
 {
+    # region 全てのメソッドを実行する
     public void Start()
     {
-         Method_0();
+        CenterMethod();
+        
+        Method_0();
+        Method_1();
+        Method_2();
+        Method_3();
+        Method_4();
+        Method_5();
+        Method_6();
+        Method_7();
+        Method_8();
+        Method_9();
+        Method_10();
+        Method_11();
+        Method_12();
+        Method_13();
+        Method_14();
+        Method_15();
+        Method_16();
+        Method_17();
+        Method_18();
+        Method_19();
+        Method_20();
+        Method_21();
+        Method_22();
+        Method_23();
+        Method_24();
+        Method_25();
+        Method_26();
+        Method_27();
+        Method_28();
+        Method_29();
+        Method_30();
+        Method_31();
+        Method_32();
+        Method_33();
+        Method_34();
+        Method_35();
+        Method_36();
+        Method_37();
+        Method_38();
+        Method_39();
+        Method_40();
+        Method_41();
+        Method_42();
+        Method_43();
+        Method_44();
+        Method_45();
+        Method_46();
+        Method_47();
+        Method_48();
+        Method_49();
+        Method_50();
+        Method_51();
+        Method_52();
+        Method_53();
+        Method_54();
+        Method_55();
+        Method_56();
+        Method_57();
+        Method_58();
+        Method_59();
+        Method_60();
     }
 
+    #endregion
     
+    private void CenterMethod()
+    {
+        Debug.Log("this is the center method");
+    }
     
     private void Method_0()
     {
         Debug.Log("Method_0");
+    }
+    
+    private void Method_1()
+    {
+        Debug.Log("Method_1");
+    }
+    
+    private void Method_2()
+    {
+        Debug.Log("Method_2");
+    }
+    
+    private void Method_3()
+    {
+        Debug.Log("Method_3");
+    }
+    
+    private void Method_4()
+    {
+        Debug.Log("Method_4");
+    }
+    
+    private void Method_5()
+    {
+        Debug.Log("Method_5");
+    }
+    
+    private void Method_6()
+    {
+        Debug.Log("Method_6");
+    }
+    
+    private void Method_7()
+    {
+        Debug.Log("Method_7");
+    }
+    
+    private void Method_8()
+    {
+        Debug.Log("Method_8");
+    }
+    
+    private void Method_9()
+    {
+        Debug.Log("Method_9");
+    }
+    
+    private void Method_10()
+    {
+        Debug.Log("Method_10");
+    }
+    
+    private void Method_11()
+    {
+        Debug.Log("Method_11");
+    }
+    
+    private void Method_12()
+    {
+        Debug.Log("Method_12");
+    }
+    
+    private void Method_13()
+    {
+        Debug.Log("Method_13");
+    }
+    
+    private void Method_14()
+    {
+        Debug.Log("Method_14");
+    }
+    
+    private void Method_15()
+    {
+        Debug.Log("Method_15");
+    }
+    
+    private void Method_16()
+    {
+        Debug.Log("Method_16");
+    }
+    
+    private void Method_17()
+    {
+        Debug.Log("Method_17");
+    }
+    
+    private void Method_18()
+    {
+        Debug.Log("Method_18");
+    }
+    
+    private void Method_19()
+    {
+        Debug.Log("Method_19");
+    }
+    
+    private void Method_20()
+    {
+        Debug.Log("Method_20");
+    }
+    
+    private void Method_21()
+    {
+        Debug.Log("Method_21");
+    }
+    
+    private void Method_22()
+    {
+        Debug.Log("Method_22");
+    }
+    
+    private void Method_23()
+    {
+        Debug.Log("Method_23");
+    }
+    
+    private void Method_24()
+    {
+        Debug.Log("Method_24");
+    }
+    
+    private void Method_25()
+    {
+        Debug.Log("Method_25");
+    }
+    
+    private void Method_26()
+    {
+        Debug.Log("Method_26");
+    }
+    
+    private void Method_27()
+    {
+        Debug.Log("Method_27");
+    }
+    
+    private void Method_28()
+    {
+        Debug.Log("Method_28");
+    }
+    
+    private void Method_29()
+    {
+        Debug.Log("Method_29");
+    }
+    
+    private void Method_30()
+    {
+        Debug.Log("Method_30");
+    }
+    
+    private void Method_31()
+    {
+        Debug.Log("Method_31");
+    }
+    
+    private void Method_32()
+    {
+        Debug.Log("Method_32");
+    }
+    
+    private void Method_33()
+    {
+        Debug.Log("Method_33");
+    }
+    
+    private void Method_34()
+    {
+        Debug.Log("Method_34");
+    }
+    
+    private void Method_35()
+    {
+        Debug.Log("Method_35");
+    }
+    
+    private void Method_36()
+    {
+        Debug.Log("Method_36");
+    }
+    
+    private void Method_37()
+    {
+        Debug.Log("Method_37");
+    }
+    
+    private void Method_38()
+    {
+        Debug.Log("Method_38");
+    }
+    
+    private void Method_39()
+    {
+        Debug.Log("Method_39");
+    }
+    
+    private void Method_40()
+    {
+        Debug.Log("Method_40");
+    }
+    
+    private void Method_41()
+    {
+        Debug.Log("Method_41");
+    }
+    
+    private void Method_42()
+    {
+        Debug.Log("Method_42");
+    }
+    
+    private void Method_43()
+    {
+        Debug.Log("Method_43");
+    }
+    
+    private void Method_44()
+    {
+        Debug.Log("Method_44");
+    }
+    
+    private void Method_45()
+    {
+        Debug.Log("Method_45");
+    }
+    
+    private void Method_46()
+    {
+        Debug.Log("Method_46");
+    }
+    
+    private void Method_47()
+    {
+        Debug.Log("Method_47");
+    }
+    
+    private void Method_48()
+    {
+        Debug.Log("Method_48");
+    }
+    
+    private void Method_49()
+    {
+        Debug.Log("Method_49");
+    }
+    
+    private void Method_50()
+    {
+        Debug.Log("Method_50");
+    }
+    
+    private void Method_51()
+    {
+        Debug.Log("Method_51");
+    }
+    
+    private void Method_52()
+    {
+        Debug.Log("Method_52");
+    }
+    
+    private void Method_53()
+    {
+        Debug.Log("Method_53");
+    }
+    
+    private void Method_54()
+    {
+        Debug.Log("Method_54");
+    }
+    
+    private void Method_55()
+    {
+        Debug.Log("Method_55");
+    }
+    
+    private void Method_56()
+    {
+        Debug.Log("Method_56");
+    }
+    
+    private void Method_57()
+    {
+        Debug.Log("Method_57");
+    }
+    
+    private void Method_58()
+    {
+        Debug.Log("Method_58");
+    }
+    
+    private void Method_59()
+    {
+        Debug.Log("Method_59");
+    }
+    
+    private void Method_60()
+    {
+        Debug.Log("Method_60");
     }
 }
