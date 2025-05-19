2b995c4c70775a61dea5940cdd50a25a35940279
HIBIKI_5201 <takuto5201@gmail.com>
2025-05-19 23:12:08 +0900

    [update]プロパティで遊ぶ

--- Assets/Student/Number_0/SampleScript_0.cs
+++ Assets/Student/Number_0/SampleScript_0.cs
@@ -1,13 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SampleScript_0 : MonoBehaviour
 {
+    [Header("パラメータ")]
+    
     [SerializeField] private string _text = "text";
-  
-    private void Start()
+    
+    # region テキスト表示のパラメータ
+    
+    [Header("テキストの表示")]
+    
+    [SerializeField, Range(1, 512)] private int _fontSize = 128;
+    [SerializeField] private Color _color = Color.white;
+    [SerializeField] private TextAnchor _anchor = TextAnchor.MiddleCenter;
+
+    #endregion
+    private void OnGUI()
     {
-        Debug.Log(_text);
+        // フォントサイズなどのスタイルを設定
+        GUIStyle style = new (GUI.skin.label);
+        style.fontSize = _fontSize;
+        style.normal.textColor = _color;
+        style.alignment = _anchor;
+
+        // 画面の中央に配置するRect
+        Rect rect = new Rect(
+            position: Vector2.zero,
+            size: new Vector2(Screen.width, Screen.height)
+        );
+
+        // テキストを描画
+        GUI.Label(rect, _text, style);
     }
 }
